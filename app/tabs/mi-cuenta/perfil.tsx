993460fe8224e89c7f0fddcf66a077e5e9f9e976
ExequielMontero <exeee2015@gmail.com>
2025-05-15 09:12:57 -0300

    Modificando cuestiones del login

--- app/tabs/mi-cuenta/perfil.tsx
+++ app/tabs/mi-cuenta/perfil.tsx
@@ -1,34 +1,38 @@
-import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 //import QRCode from 'react-native-qrcode-svg';
-import { useRouter } from 'expo-router';
-
+import { useRouter } from "expo-router";
+import { useAuth } from "../../../context/authContext";
 const ProfileScreen = () => {
-
+  const { logout } = useAuth();
+  const router = useRouter();
   const profileData = {
-    name: 'Ariel Perez',
-    userType: 'Cazador | Pescador',
-    qrValue: 'arielperez', 
+    name: "Ariel Perez",
+    userType: "Cazador | Pescador",
+    qrValue: "arielperez",
   };
-  
+
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
         <Ionicons name="person-circle" size={100} color="#333" />
       </View>
-        {/* Datos */}
-        <Text style={styles.name}>{profileData.name}</Text>
-        <Text style={styles.userType}>{profileData.userType}</Text>
-        {/* QR Code 
+      {/* Datos */}
+      <Text style={styles.name}>{profileData.name}</Text>
+      <Text style={styles.userType}>{profileData.userType}</Text>
+      {/* QR Code 
         <View style={styles.qrContainer}>
           <QRCode value={profileData.qrValue} size={150} />
         </View> */}
-        <TouchableOpacity
-        >
-          <Text style={{ color: 'red' }}>Cerrar sesión</Text>
-        </TouchableOpacity>
-
+      <TouchableOpacity
+        onPress={() => {
+          logout();
+          router.replace("./index"); // o donde querés mandar al usuario
+        }}
+      >
+        <Text style={{ color: "red" }}>Cerrar sesión</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -36,35 +40,34 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   profileContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   name: {
     fontSize: 22,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: "bold",
+    color: "#333",
     marginBottom: 5,
   },
   userType: {
     fontSize: 16,
-    color: '#888',
+    color: "#888",
     marginBottom: 10,
   },
   qrContainer: {
     marginTop: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
 export default ProfileScreen;
 
 function useState(arg0: string): [any, any] {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
-
